c803669028661242877f2a63993c5d8ea120e299
Oh Olya <chanfoxx@yandex.ru>
2023-11-01 23:54:39 +0300

    create command - loaddata_category.py

---
+++ catalog/management/commands/loaddata_category.py
@@ -0,0 +1,17 @@
+import json
+from catalog.models import Category
+from config.settings import CATEGORY_FILE
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Загружает данные из фикстуры в таблицу категорий."""
+    def handle(self, *args, **kwargs):
+        with open(CATEGORY_FILE, encoding='utf-8') as file:
+            data = json.load(file)
+            for category in data:
+                Category.objects.create(
+                    title=category['fields']['title'],
+                    description=category['fields'].get('description', 'Описание'),
+                    image=category['fields'].get('image', None),
+                )
