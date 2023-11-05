1ea158a4f0b99c26cb34526cc6939134845d43ff
Oh Olya <chanfoxx@yandex.ru>
2023-11-05 18:49:40 +0300

    create CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,63 +1,63 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from pytils.translit import slugify
 from catalog.models import Product, Category, Contact
+from django.views.generic import ListView, DetailView, TemplateView
 
 
-def main(request):
-    """Главная страница."""
-    five_products = Product.objects.all()[:2]
+class MainTemplateView(TemplateView):
+    template_name = 'catalog/main.html'
 
-    context = {
-        'title': 'SkyStore',
-        'object_list': five_products,
-    }
-    return render(request, 'catalog/main.html', context)
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        five_products = Product.objects.all()[:6]
+        context_data['title'] = 'SkyStore'
+        context_data['object_list'] = five_products
+
+        return context_data
 
 
-def contact(request):
-    """Контактная страница."""
-    contact_list = Contact.objects.all()
+class ContactTemplateView(TemplateView):
+    template_name = 'catalog/contact.html'
+    extra_context = {'title': 'Контакты'}
 
-    context = {
-        'object_list': contact_list,
-        'title': 'Контакты'
-    }
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         message = request.POST.get('message')
+
+        contact = Contact(name=name, phone=phone, email=email, message=message)
+        contact.save()
+
         print(f'У Вас новое сообщение от {name}'
               f'(phone: {phone}, e-mail: {email}): "{message}"')
 
-    return render(request, 'catalog/contact.html', context)
+        return super().get(request, *args, **kwargs)
 
 
+class CategoryListView(ListView):
+    model = Category
 
 
-def categories(request):
-    """Страница продуктовых категорий."""
-    category_list = Category.objects.all()
-    context = {
-        'object_list': category_list,
-    }
-    return render(request, 'catalog/categories.html', context)
+class ProductListView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category=self.kwargs.get('pk'))
+
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        category = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['category'] = category
+
+        return context_data
 
 
-def goods(request, pk):
-    """Страница с продуктами по одной категории."""
-    category = Category.objects.get(pk=pk)
-    products = Product.objects.filter(category=category)
-    context = {
-        'category': category,
-        'object_list': products,
-    }
-    return render(request, 'catalog/goods.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
-
-def product(request, pk):
-    """Страница с определенным товаром."""
-    one_product = Product.objects.get(pk=pk)
-    context = {
-        'object': one_product,
-    }
-    return render(request, 'catalog/product.html', context)
