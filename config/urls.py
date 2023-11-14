a8bbf5dc50c94b2353ef4bd32bb97c5245788f25
Oh Olya <chanfoxx@yandex.ru>
2023-11-15 01:10:19 +0300

    add app users urls

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('users/', include('users.urls', namespace='users')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
