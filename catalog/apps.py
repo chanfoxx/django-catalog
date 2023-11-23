634da91a87a71fc9bbb385eda080fdd6429c6537
Oh Olya <chanfoxx@yandex.ru>
2023-11-24 00:41:36 +0300

    add ready for moderator group

--- catalog/apps.py
+++ catalog/apps.py
@@ -5,3 +5,13 @@ class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
     verbose_name = 'Каталог'
+
+    def ready(self) -> None:
+        from django.contrib.auth.models import Group, Permission
+
+        moderator_group, _ = Group.objects.get_or_create(name='Модератор')
+        moderator_permission = Permission.objects.filter(
+            codename__in=['cancel_published_status', 'change_category', 'change_description']
+        )
+        for perm in moderator_permission:
+            moderator_group.permissions.add(perm)
