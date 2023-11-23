4e15f72ba07e3d06112d79c6effc3e94c7d4d7b1
Oh Olya <chanfoxx@yandex.ru>
2023-11-24 00:50:55 +0300

    add mailing app

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
     path('users/', include('users.urls', namespace='users')),
+    path('mailing/', include('mailing.urls', namespace='mailing')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
