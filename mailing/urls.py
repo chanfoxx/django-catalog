c0c7cd989fc0de528fd995dcaef220e881faf2bb
Oh Olya <chanfoxx@yandex.ru>
2023-11-24 00:52:41 +0300

    create mailing views

---
+++ mailing/urls.py
@@ -0,0 +1,33 @@
+"""
+    URL configuration for config project.
+
+    The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    Examples:
+    Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from mailing.apps import MailingConfig
+from mailing.views import (MailingListView, MailingCreateView, MessageCreateView,
+                           ClientCreateView, MailingDetailView, MailingUpdateView)
+
+
+app_name = MailingConfig.name
+
+
+urlpatterns = [
+    path('', MailingListView.as_view(), name='mailing_list'),
+    path('create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('message/', MessageCreateView.as_view(), name='message_create'),
+    path('client/', ClientCreateView.as_view(), name='client_create'),
+    path('detail/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
+    path('edit/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+]
