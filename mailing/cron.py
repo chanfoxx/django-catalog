8b3c46752372383b14dff30880c3a4be9bba019a
Oh Olya <chanfoxx@yandex.ru>
2023-12-01 14:41:56 +0300

    change functions

--- mailing/cron.py
+++ mailing/cron.py
@@ -1,21 +1,52 @@
+from smtplib import SMTPException
 from django.utils import timezone
 from django.conf import settings
 from django.core.mail import send_mail
-from mailing.models import MailingSettings
+from mailing.models import MailingSettings, MailingLogs
+
+
+def change_status_launched():
+    """Меняет статус в состояние - запущено."""
+    current_datetime = timezone.now()
+    mailing = MailingSettings.objects.filter(status='CR', start_time__lte=current_datetime,
+                                             end_time__gte=current_datetime)
+    for mail in mailing:
+        mail.status = 'LC'
+        mail.save()
 
 
 def my_scheduled_job():
-    current_time = timezone.now().time()
-    # mailing = MailingSettings.objects.filter(start_time__lte=current_time,
-    #                                          end_time__gte=current_time)
-    mailing = MailingSettings.objects.all()
-    email = 'chanfoxx@yandex.ru'
+    """."""
+    mailing = MailingSettings.objects.filter(status='LC')
+
     for mail in mailing:
-        if mail.start_time <= current_time <= mail.end_time:
-            # for client in mail.client.all():
-            send_mail(
-                subject=mail.message.subject,
-                message=mail.message.message,
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[email]
-            )
+        for client in mail.client.all():
+            print('пока ОК')
+            try:
+                send_mail(
+                    subject=mail.message.subject,
+                    message=mail.message.body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[client.email],
+                    fail_silently=False,
+                )
+                print('ОК')
+                MailingLogs.objects.create(status=1, mail_server_response='OK',
+                                           client=client, mailing_settings=mail)
+            except SMTPException(OSError) as e:
+                print('ВАЩЕ НЕ ОК')
+                MailingLogs.objects.create(status=2, mail_server_response=e)
+
+
+        # if
+        # MailingLogs.objects.create(status=, mail_server_response=)
+        # if MailingLogs.date.timedate.date() == timezone.now().date():
+
+
+def change_status_completed():
+    """Меняет статус в состояние - завершено."""
+    mailing = MailingSettings.objects.filter(status='LC')
+
+    for mail in mailing:
+        mail.status = 'CL'
+        mail.save()
