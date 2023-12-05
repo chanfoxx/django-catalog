bbbcfcb22fc4539ae3e4e41315b3f8be94eaf33e
Oh Olya <chanfoxx@yandex.ru>
2023-12-05 20:47:20 +0300

    add class Meta. permission

--- users/models.py
+++ users/models.py
@@ -15,6 +15,14 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+        permissions = [
+            ('set_is_active', 'Может блокировать пользователей сервиса'),
+        ]
+
 
 class EmailVerification(models.Model):
     """Модель верификации почты."""
