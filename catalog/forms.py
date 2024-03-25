26837bb120cb3ad89611640f38ee95867a1d0d8a
Oh Olya <chanfoxx@yandex.ru>
2024-03-25 16:51:33 +0300

    add spaces for forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,15 +1,17 @@
 from django import forms
+
 from catalog.models import Product, Blog, Version, Contact
 
 
 class ProductForm(forms.ModelForm):
-    """Форма для товара."""
+    """ Форма для товара. """
+
     class Meta:
         model = Product
         exclude = ('creator',)
 
     def clean_title(self):
-        """Валидация названия товара."""
+        """ Валидация названия товара. """
         danger_list = [
             'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
             'бесплатно', 'обман', 'полиция', 'радар'
@@ -18,12 +20,14 @@ class ProductForm(forms.ModelForm):
 
         for danger in danger_list:
             if danger in cleaned_data.lower():
-                raise forms.ValidationError(f'Слово "{danger}" не может использоваться.')
+                raise forms.ValidationError(
+                    f'Слово "{danger}" не может использоваться.'
+                )
 
         return cleaned_data
 
     def clean_description(self):
-        """Валидация описания товара."""
+        """ Валидация описания товара. """
         danger_list = [
             'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
             'бесплатно', 'обман', 'полиция', 'радар'
@@ -32,34 +36,40 @@ class ProductForm(forms.ModelForm):
 
         for danger in danger_list:
             if danger in cleaned_data.lower():
-                raise forms.ValidationError(f'Слово "{danger}" не может использоваться.')
+                raise forms.ValidationError(
+                    f'Слово "{danger}" не может использоваться.'
+                )
 
         return cleaned_data
 
 
 class VersionForm(forms.ModelForm):
-    """Форма для версий."""
+    """ Форма для версий. """
+
     class Meta:
         model = Version
         fields = '__all__'
 
 
 class ContactForm(forms.ModelForm):
-    """Форма обратной связи."""
+    """ Форма обратной связи. """
+
     class Meta:
         model = Contact
         fields = '__all__'
 
 
 class ModeratorForm(forms.ModelForm):
-    """Форма для модератора."""
+    """ Форма для модератора. """
+
     class Meta:
         model = Product
         fields = ('description', 'category', 'is_published',)
 
 
 class BlogForm(forms.ModelForm):
-    """Форма для блоговых записей."""
+    """ Форма для блоговых записей. """
+
     class Meta:
         model = Blog
         exclude = ('slug', 'view_count', 'creator',)
