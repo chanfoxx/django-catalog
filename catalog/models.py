171d962be2fde30156f7fadd3ea5ff401e6be27f
Oh Olya <chanfoxx@yandex.ru>
2024-03-25 16:52:32 +0300

    edit space

--- catalog/models.py
+++ catalog/models.py
@@ -1,13 +1,13 @@
 from typing import Optional
+
 from django.db import models
 from django.conf import settings
-
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-    """Модель товара."""
+    """ Модель товара. """
     title = models.CharField(max_length=200)
     description = models.TextField(**NULLABLE)
     category = models.ForeignKey('catalog.Category', on_delete=models.CASCADE)
@@ -15,20 +15,25 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_add = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
-                                **NULLABLE, verbose_name='Продавец')
+    is_published = models.BooleanField(
+        default=False,
+        verbose_name='Опубликовано')
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name='Продавец')
 
     @property
     def active_version(self) -> Optional['Version']:
         return self.version_set.filter(is_active=True).last()
 
     def __str__(self) -> str:
-        """Возвращает строковое представление о классе товара."""
+        """ Возвращает строковое представление о классе товара. """
         return f'{self.title}'
 
     class Meta:
-        """Метаданные для модели товара."""
+        """ Метаданные для модели товара. """
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
 
@@ -40,70 +45,89 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    """Модель категории."""
+    """ Модель категории. """
     title = models.CharField(max_length=200)
     description = models.TextField(**NULLABLE)
     image = models.ImageField(upload_to='images/category/', **NULLABLE)
 
     def __str__(self) -> str:
-        """Возвращает строковое представление о классе категории."""
+        """ Возвращает строковое представление о классе категории. """
         return f'{self.title}'
 
     class Meta:
-        """Метаданные для модели категории."""
+        """ Метаданные для модели категории. """
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
 
 class Contact(models.Model):
-    """Модель контактных данных."""
+    """ Модель контактных данных. """
     name = models.CharField(max_length=100, verbose_name='Имя')
     phone = models.CharField(max_length=50, verbose_name='Номер телефона')
     email = models.EmailField(max_length=100, verbose_name='E-mail')
     message = models.TextField(verbose_name='Сообщение')
 
     def __str__(self) -> str:
-        """Возвращает строковое представление о классе контактных данных."""
+        """ Возвращает строковое представление о классе контактных данных. """
         return f'{self.name} (phone: {self.phone})'
 
     class Meta:
-        """Метаданные для модели контактных данных."""
+        """ Метаданные для модели контактных данных. """
         verbose_name = 'Контактные данные'
         verbose_name_plural = 'Контактные данные'
 
 
 class Version(models.Model):
-    """Модель версии."""
+    """ Модель версии. """
     product = models.ForeignKey('catalog.Product', on_delete=models.PROTECT)
-    version_number = models.CharField(max_length=10, verbose_name='Номер версии')
+    version_number = models.CharField(
+        max_length=10,
+        verbose_name='Номер версии'
+    )
     title = models.CharField(max_length=150, verbose_name='Название версии')
-    is_active = models.BooleanField(default=False, verbose_name='Признак активной версии')
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Признак активной версии'
+    )
 
     def __str__(self) -> str:
-        """Возвращает строковое представление о классе версий."""
+        """ Возвращает строковое представление о классе версий. """
         return f'{self.version_number} {self.title}'
 
     class Meta:
-        """Метаданные для модели контактных данных."""
+        """ Метаданные для модели контактных данных. """
         verbose_name = 'Версия'
         verbose_name_plural = 'Версии'
 
 
 class Blog(models.Model):
-    """Модель блоговой записи."""
+    """ Модель блоговой записи. """
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     slug = models.CharField(max_length=150, verbose_name='Slug', **NULLABLE)
     description = models.TextField(verbose_name='Содержимое', **NULLABLE)
-    preview = models.ImageField(upload_to='images/blog/', verbose_name='Изображение', **NULLABLE)
+    preview = models.ImageField(
+        upload_to='images/blog/',
+        verbose_name='Изображение',
+        **NULLABLE
+    )
     creation_date = models.DateTimeField(auto_now_add=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
     view_count = models.IntegerField(default=0, verbose_name='Просмотры')
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
-                                **NULLABLE, verbose_name='Автор')
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name='Автор'
+    )
 
     def __str__(self):
+        """ Возвращает строковое представление о классе блога. """
         return f'{self.title}'
 
     class Meta:
+        """ Метаданные для модели блога. """
         verbose_name = 'Блоговая запись'
         verbose_name_plural = 'Блоговые записи'
